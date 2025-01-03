823eee7f47f76d691a3b60d16a70692d794d3c22
Bagus Dewantoro <bagusdewantoro@gmail.com>
2025-01-04 01:06:05 +0700

    helicopter, drum pattern, loop volume

--- KidA.rb
+++ KidA.rb
@@ -1,26 +1,33 @@
 ##| Kid A - Radiohead
 
 
+# Change value to "1" to activate
+amp_piano = 0
+amp_bass_drum = 0
+amp_bell = 1
+
+
+
 ##| =========== SOUND DEFINITION ===========
 
 define :rhodes1 do |note|
   with_fx :compressor do
     with_fx :flanger, phase_offset: 0.3, mix: 0.01 do
-      synth :sine, note: note, attack: 0.1, decay: 0.5, sustain_level: 0.35, release: 0.8, amp: 0.45
-      synth :pretty_bell, note: note, amp: 0.02
+      synth :sine, note: note, attack: 0.1, decay: 0.5, sustain_level: 0.35, release: 0.8, amp: (0.45*amp_piano)
+      synth :pretty_bell, note: note, amp: (0.07*amp_piano)
     end
   end
 end
 
 define :piano1 do |note|
   with_fx :reverb do
-    synth :beep, note: note, attack: 0.01, decay: 0.5, sustain_level: 0.5, release: 1.4, amp: 0.7
+    synth :beep, note: note, attack: 0.01, decay: 0.5, sustain_level: 0.5, release: 1.4, amp: (0.7*amp_piano)
   end
 end
 
 define :marimba do |note|
-  with_fx :echo, phase: 0.5, mix: 0.6, pre_amp: 1.0, release: 4 do
-    synth :beep, note: note, attack: 0, decay: 0.5, sustain_level: 1, release: 1, amp: 1.3
+  with_fx :echo, phase: 0.5, mix: 0.5, pre_amp: 1.0, release: 7 do
+    synth :beep, note: note, attack: 0, decay: 0.5, sustain_level: 1, release: 1, amp: (1.5*amp_bass_drum)
   end
 end
 
@@ -50,8 +57,10 @@ bassNote1 = [
 
 
 drumTomSleep = [
-  1, 1, 0.5, 0.75, 2.25, 0.25, 0.5, 1.5, 0.75, # 8.5 beat
-  0.75, 0.75, (2.to_f/3), (4.to_f/3), 1.75, 0.25, (2.to_f/3), (2.to_f/3), (1.to_f/6), (3.to_f/6), # 7.5 beat
+  1, 1, 0.5, 0.75, 0.25, 0.5,        1.25, 0.25, 0.5, 0.5, 0.75, 0.25, 0.5,       # 8 beat
+  0.5, 0.25, 0.5, 0.25, 0.5,         1.75, 0.25, (4.to_f/6), (4.to_f/6), (1.to_f/6), (3.to_f/6), # 8 beat
+  1, 1, 0.5, 0.75, 0.25, 0.5,        1.25, 0.25, 0.5, 0.5, 0.75, 0.25, 0.5,       # 8 beat
+  (2.to_f/3), (2.to_f/3), (2.to_f/3),   1.75, 0.25, (4.to_f/6), (4.to_f/6), (4.to_f/6), # 8 beat
 ].ring
 
 
@@ -66,33 +75,55 @@ use_bpm 110
 
 in_thread(name: :Helicopter) do
   1.times do
-    rate_values = (line 15, 8, steps: 18) + (line 8, 10, steps: 10) + (line 10, 14, steps: 18)
-    with_fx :gverb, mix: 0.3 do
+    rate_values1 = (line 15, 8, steps: 40) + (line 8, 10, steps: 20) + (line 10, 14, steps: 28)
+    rate_values2 = (line 10, 15, steps: 24)
+    rate_values3 = (line 14, 23, steps: 15)
+    with_fx :gverb, mix: 0.9 do
       with_fx :echo, phase: 0.2, mix: 1 do
-        rate_values.each do |rate|
+        rate_values1.each do |rate|
           sample :bd_pure, amp: 0.2, rate: rate
           sleep 0.3
+        end
+        sleep 5
+        rate_values2.each do |rate|
+          sample :bd_pure, amp: 0.2, rate: rate
+          sleep 0.3
+        end
+        sleep 2
+        rate_values3.each do |rate|
+          sample :bd_pure, amp: 0.1, rate: rate
+          sleep 0.10
+        end
+        sleep 2
+        rate_values3.each do |rate|
+          sample :bd_pure, amp: 0.04, rate: (rate*1.2)
+          sleep 0.07
         end
       end
     end
   end
 end
 
-1.times do
-  amp_values = (line 0.02, 0.6, steps: 10) + (line 0.6, 0.5, steps: 2)
-  with_fx :flanger, phase_offset: 0.3, mix: 0.2 do
-    amp_values.each do |amp|
-      synth :sine, note: :d6, amp: amp
-      sleep 1
+live_loop :bellIntro do
+  1.times do
+    amp_values = (line 0.02, 0.6, steps: 10) + (line 0.6, 0.5, steps: 6)
+    with_fx :flanger, phase_offset: 0.3, mix: 0.2 do
+      amp_values.each do |amp|
+        synth :sine, note: :d6, amp: (amp*amp_bell)
+        sleep 1
+      end
     end
   end
 end
 
-live_loop :beat do
-  16.times do
-    play 1, amp: 0
+live_loop :bellEnd do
+  end_values = (line 0.5, 2, steps: 3)
+  sleep 12.7
+  end_values.each do |amp|
+    sample :drum_cymbal_closed, amp: (amp*amp_bell), attack: 0, rate: -1
     sleep 1
   end
+  sleep 0.3
 end
 
 live_loop :pianoIntro do
@@ -107,18 +138,15 @@ live_loop :bassIntro do
 end
 
 live_loop :bassDrum1 do
-  sample :bd_ada, amp: 0.7 , release: 0
+  sample :bd_ada, amp: (0.7*amp_bass_drum) , release: 0
   sleep 1
-end
-
-live_loop :bassDrum2 do
-  sleep 1
-  sample :bd_sone, amp: 0.5 , release: 0
+  sample :bd_ada, amp: (0.7*amp_bass_drum) , release: 0
+  sample :bd_sone, amp: (0.5*amp_bass_drum) , release: 0
   sleep 1
 end
 
 live_loop :tomDrum do
-  sample :bd_haus, rate: 1.5 , amp: rrand(1.0, 1.2), lpf: rrand(65, 75)
+  sample :bd_haus, rate: 1.5 , amp: rrand((1.0*amp_bass_drum), (1.2*amp_bass_drum)), lpf: rrand(65, 75)
   sleep drumTomSleep.tick(:four)
 end
 
